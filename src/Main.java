48ff143ca5a4032e9cbc4673c2608afd9245b88d
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2022-10-16 22:45:25 -0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        String str1 = "CAKE";
+        String str2 = new String("CAKE");
+
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+    }
 }
